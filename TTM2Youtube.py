11330b078113178a118e5c67b71705b14015df79
Ali Mirjamali <ali.mirjamali@gmail.com>
2015-03-01 21:45:50 +0330

    Building the foundation for code

--- TTM2Youtube.py
+++ TTM2Youtube.py
@@ -6,7 +6,7 @@
 #              --- Copyright Ali Mirjamali (2015) ---
 #
 
-import httplib, urllib
+import httplib, urllib, urllib2
 import time
 
 # Currently Tehran Traffic Map is not available via a Domain. Here is the IP:
@@ -18,6 +18,8 @@ myMapURL = "/TTCCTrafficWebSite/UploadedFiles/WebTrafficImages/Web0.png"
 etag_old=""
 etag_new=""
 
+# File index
+myFileNum = 1
 
 while True:
 	# Connect to the server and retrieve headers
@@ -26,15 +28,22 @@ while True:
 	myResponse = myHTTPConnection.getresponse()
 	myHeaders = myResponse.getheaders()
 	# Store the new ETag header in etag_new
-	for header in myHeaders:
-		if header[0] == 'etag':
-			etag_new=header[1]
-			break
+	etag_new = myResponse.getheader("etag")
 	# Do we have a new refreshed map?
 	if etag_new != etag_old:
+		
+		# Code for sanity check. To be removed later
 		etag_old=etag_new
 		for header in myHeaders:
 			print header
+		
+		# Read the PNG file and store in myMap val, write to ./tmp/########.png
+		myMap = urllib2.urlopen("http://" + myHost + myMapURL).read()
+		myFile = open("./tmp/" + str(myFileNum).zfill(8) + ".png", 'wb')
+		myFile.write(myMap)
+		myFile.close()
+		# Increase file index for next map image	
+		myFileNum+=1
 	else:
 		# If not, wait 15 seconds and check again
 		time.sleep(15)
