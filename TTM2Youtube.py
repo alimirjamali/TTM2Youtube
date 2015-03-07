89d124b32164ddbff3e97a83e2b8b3486184835a
Ali Mirjamali <ali.mirjamali@gmail.com>
2015-03-08 01:38:49 +0330

    Rewriting the avconv subprocess

--- TTM2Youtube.py
+++ TTM2Youtube.py
@@ -10,6 +10,7 @@
 import httplib, urllib, urllib2
 import time, datetime
 import tempfile
+import subprocess
 
 # Currently Tehran Traffic Map is not available via a Domain address. Here is the server's IP:
 myHost = "31.24.237.150"
@@ -18,14 +19,37 @@ myHost = "31.24.237.150"
 myMapURL = "/TTCCTrafficWebSite/UploadedFiles/WebTrafficImages/Web0.png"
 
 # ETags to check if the map image file is updated on server
-etag_old=""
-etag_new=""
+etag_old = ""
+etag_new = ""
+
+# We will upload one video per day
+today = datetime.date.today()
 
 # Create tmp directory
 myTempDir = tempfile.mkdtemp(prefix="TTM2Youtube")
 
 # File index on temp directory
 myFileNum = 1
+
+# Generate video for upload by calling avconv via a subprocess
+def myFuncGenerateVid():
+	myCommandLine = [
+		'avconv',
+		'-y',
+		'-i',
+		myTempDir + "/%08d.jpg",
+		'-r', '24',
+		myTempDir + "/output.mp4"]
+	pipe = subprocess.Popen(myCommandLine, stdin=subprocess.PIPE, stderr=subprocess.PIPE)
+	return
+
+# Upload the video to Youtube
+def myFuncUpload2Youtube():
+	return
+
+# Erase temp files
+def myFuncClearTemp():
+	return
 
 # Read the PNG file from site and write to ########.png in temp directory
 def myFuncGetMap(index):
@@ -46,6 +70,7 @@ def myFuncGetETag():
 	return myResponse.getheader("etag")
 
 while True:
+
 	# Store the new ETag header in etag_new
 	etag_new = myFuncGetETag()
 	# Do we have a new refreshed map?
@@ -58,7 +83,12 @@ while True:
 		myFileNum+=1
 		etag_old=etag_new
 
+	# Upload the video on date change and clear temp, otherwise wait for 15 seconds to check for a new map
+	if today != datetime.date.today():
+		myFuncGenerateVid()
+		myFuncUpload2Youtube()
+		myFuncClearTemp()
+		today = datetime.date.today()
+		myFileNum = 1
 	else:
-		# If there is no any new map image to download, wait 15 seconds and check again
 		time.sleep(15)
-		continue
